2421c923fd75aff98abc270d2e409859572212db
Ibrahim Mostafa <ibrahim@Ibrahims-Mac.local>
2017-11-18 19:13:28 +0200

    Login

--- twitterApp/ViewController.swift
+++ twitterApp/ViewController.swift
@@ -13,8 +13,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        login()
     }
    
-
+    func login(){
+        let logInButton = TWTRLogInButton(logInCompletion: { session, error in
+            if (session != nil) {
+                print("signed in as \(session?.userName)");
+                
+            } else {
+                print("error: \(error?.localizedDescription)");
+            }
+            //print ( "-------------->" , Twitter.sharedInstance().sessionStore )
+            
+        })
+        logInButton.center = self.view.center
+        self.view.addSubview(logInButton)
+        
+    }
 }
 
